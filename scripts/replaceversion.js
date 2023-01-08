d162cc733ec47918523d3d6ebca37365c8af9212
nopeless <38830903+nopeless@users.noreply.github.com>
2023-01-07 21:35:59 -0600

    fix script

--- scripts/replaceversion.js
+++ scripts/replaceversion.js
@@ -2,10 +2,8 @@ import fs from "fs";
 import p from "../package.json" assert { type: "json" };
 
 fs.writeFileSync(
-  "./src/README.txt",
-  fs
-    .readFileSync("./src/README.txt", "utf8")
-    .replace(/\{\{VERSION\}\}/g, p.version)
+  "./README.txt",
+  fs.readFileSync("./README.txt", "utf8").replace(/\{\{VERSION\}\}/g, p.version)
 );
 
 console.log("Successfuly replaced");
